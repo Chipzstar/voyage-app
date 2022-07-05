57ea968f6b9d80f785fdb17f6e0debcc5b4fee97
Chisom Oguibe <chipzstar.dev@googlemail.com>
2022-07-05 13:41:06 +0100

    Update UI for booking page

--- apps/shipper-dashboard/components/CalendarPicker.tsx
+++ apps/shipper-dashboard/components/CalendarPicker.tsx
@@ -1,18 +1,34 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from '@mantine/core';
+import { Indicator, Modal } from '@mantine/core';
 import { Calendar } from '@mantine/dates';
+import dayjs from 'dayjs';
 
 const CalendarPicker = ({ opened, onClose, value, setValue }) => {
 
 	useEffect(() => {
-		onClose()
-	}, [])
+		console.log(value)
+	}, []);
 
 	return (
-		<Modal opened={opened} onClose={onClose} centered size="md" withCloseButton={false} closeOnEscape closeOnClickOutside>
-			<div className="flex justify-center items-center">
-				<Calendar value={value} onChange={setValue} fullWidth/>
+		<Modal opened={opened} onClose={onClose} centered size='md' withCloseButton={false} closeOnEscape closeOnClickOutside>
+			<div className='flex justify-center items-center'>
+				<Calendar
+					firstDayOfWeek="sunday"
+					value={value}
+					onChange={setValue}
+					fullWidth
+					renderDay={date => {
+						console.log(dayjs(date).format("DD.MM.YYYY"))
+						const disabled = value !== dayjs(date).format("DD.MM.YYYY")
+						const day = date.getDate();
+						return (
+							<Indicator size={6} color='red' offset={8} disabled={disabled}>
+								<div>{day}</div>
+							</Indicator>
+						);
+					}}
+				/>
 			</div>
 		</Modal>
 	);
--- apps/shipper-dashboard/containers/Quotes.tsx
+++ apps/shipper-dashboard/containers/Quotes.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Quotes = () => {
 	return (
-		<div className='flex flex-col justify-center items-center space-y-12 py-5 booking-container'>
+		<div className='flex flex-col justify-center items-center space-y-8 py-5 booking-container'>
 			<h3 className="text-5xl font-light">Welcome to Voyage!</h3>
-			<p className="text-3xl">Your middle mile shipping partner optimizing your shipments.</p>
+			<p className="text-xl">Your middle mile shipping partner optimizing your shipments.</p>
 			<button className="bg-secondary-500 hover:bg-secondary-700 text-white font-medium h-12 w-48">
 				Get a new quote
 			</button>
