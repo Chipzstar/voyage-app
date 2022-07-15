76ab76e2706c03032a8c0c674c85686596d7bc97
Chipzstar <chipzstar.dev@googlemail.com>
2022-07-15 16:31:55 +0100

    remove dark theming in sidebar

--- apps/shipper-dashboard/layout/Sidebar.tsx
+++ apps/shipper-dashboard/layout/Sidebar.tsx
@@ -9,14 +9,13 @@ import { useDispatch } from 'react-redux';
 const SideMenuItem = ({ title, icon, href, isActive }) => {
 	const wrapperStyles = classNames({
 		'hover:bg-secondary-100': true,
-		'dark:hover:bg-gray-700': true,
 		'bg-secondary-100': isActive
 	});
 
 	return (
 		<li className={wrapperStyles}>
 			<Link href={href}>
-				<div role='button' className='p-4 flex items-center text-base font-normal text-gray-900 dark:text-white'>
+				<div role='button' className='p-4 flex items-center text-base font-normal text-gray-900'>
 					<img src={icon} alt='' className='w-6 h-6' />
 					<span className='ml-6 text-base md:text-lg'>{title}</span>
 				</div>
@@ -29,11 +28,11 @@ const Sidebar = () => {
 	const dispatch = useDispatch();
 	const router = useRouter();
 	return (
-		<div className='w-48 lg:w-64 h-full overflow-y-auto py-4 bg-gray-50 dark:bg-gray-800 flex flex-col border-r-2 border-gray-300'>
+		<div className='w-48 lg:w-64 h-full overflow-y-auto py-4 bg-gray-50 flex flex-col border-r-2 border-gray-300'>
 			<Link href={PATHS.HOME}>
 				<div role='button' className='flex flex-row items-center pl-4 mb-7'>
 					<img src='/static/images/favicon.svg' className='mr-3 h-6 sm:h-7' alt='Voyage Logo' />
-					<span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white mb-0.5'>voyage</span>
+					<span className='self-center text-2xl font-semibold whitespace-nowrap mb-0.5'>voyage</span>
 				</div>
 			</Link>
 			<ul className='grow space-y-4'>
@@ -46,7 +45,7 @@ const Sidebar = () => {
 
 			<div
 				role='button'
-				className='flex items-center p-4 text-base font-normal text-gray-900 dark:text-white hover:bg-secondary-100 dark:hover:bg-gray-700'
+				className='flex items-center p-4 text-base font-normal text-gray-900 hover:bg-secondary-100'
 				onClick={() => {
 					dispatch({ type: 'RESET' });
 					// @ts-ignore
