81174c9b419a00d6d4807749ab32dfab643d48e1
Chipzstar <chipzstar.dev@googlemail.com>
2022-07-25 13:25:00 +0100

    remove session redirect in homepage

--- apps/shipper-dashboard/pages/index.tsx
+++ apps/shipper-dashboard/pages/index.tsx
@@ -39,14 +39,6 @@ export function Index(props) {
 export async function getServerSideProps({ req, res }) {
 	// @ts-ignore
 	const session = await unstable_getServerSession(req, res, authOptions)
-	if (!session) {
-		return {
-			redirect: {
-				destination: PUBLIC_PATHS.LOGIN,
-				permanent: false,
-			},
-		}
-	}
 	let shipments = await prisma.shipment.findMany({
 		where: {
 			userId: {
