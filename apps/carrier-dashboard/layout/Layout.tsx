2e35176d930239123fbf133ecf17dfcec4320fc9
Chipzstar <chipzstar.dev@googlemail.com>
2022-07-26 12:47:48 +0100

    code cleanup

--- apps/carrier-dashboard/layout/Layout.tsx
+++ apps/carrier-dashboard/layout/Layout.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import {useRouter} from 'next/router';
-import {PUBLIC_PATHS} from '../utils/constants';
+import { useRouter } from 'next/router';
+import { PUBLIC_PATHS } from '../utils/constants';
 
 const Layout = ({ children }) => {
 	const router = useRouter();
 	return (
 		<div className='h-screen flex relative overflow-hidden'>
-			{!router.pathname.includes(PUBLIC_PATHS.LOGIN) && <aside className='sticky top-0 relative' aria-label='Sidebar'>
-				<Sidebar />
-			</aside>}
+			{!router.pathname.includes(PUBLIC_PATHS.LOGIN) && (
+				<aside className='sticky top-0 relative' aria-label='Sidebar'>
+					<Sidebar />
+				</aside>
+			)}
 			<main className='h-screen overflow-y-auto grow	'>{children}</main>
 		</div>
 	);
