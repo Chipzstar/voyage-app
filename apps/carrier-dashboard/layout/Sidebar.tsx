4d9d787afa8f12e33b298e72b8069cf9e67cbd08
Chipzstar <chipzstar.dev@googlemail.com>
2022-08-01 15:04:36 +0100

    fix Sidebar.tsx

--- apps/carrier-dashboard/layout/Sidebar.tsx
+++ apps/carrier-dashboard/layout/Sidebar.tsx
@@ -124,7 +124,7 @@ const Sidebar = () => {
 			submenu: true,
 			isActive: router.pathname.includes(PATHS.REPORTING),
 			menuItems: [
-				{ title: 'Basic Reports', href: PATHS.BASIC_REPORT, isActive: router.pathname === PATHS.BASIC_REPORT },
+				{ title: 'Basic Reports', href: PATHS.BASIC_REPORT, isActive: router.pathname.includes(PATHS.BASIC_REPORT) },
 				{ title: 'Fuel', href: PATHS.FUEL_REPORT, isActive: router.pathname === PATHS.FUEL_REPORT }
 			]
 		},
