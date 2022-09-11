28dcd65d2adf4c397ebe81141dd8b0352070f8ce
Chipzstar <chipzstar.dev@googlemail.com>
2022-09-11 14:51:35 +0100

    Revert "add intercom to shipper dash +"
    This reverts commit 2ede3c6481316135757b133c626461ceb5383ae7.

--- apps/shipper-dashboard/layout/Layout.tsx
+++ apps/shipper-dashboard/layout/Layout.tsx
@@ -24,6 +24,58 @@ const Layout = ({ children }) => {
 			)}
 			<main className='h-screen grow overflow-y-auto'>
 				{children}
+				{isAuthScreen && (
+					<ChatWidget
+						// `accountId` is used instead of `token` in older versions
+						// of the @papercups-io/chat-widget package (before v1.2.x).
+						// You can delete this line if you are on the latest version.
+						// accountId="8d14f8d9-7027-4af7-8fb2-14ca0712e633"
+						token='8d14f8d9-7027-4af7-8fb2-14ca0712e633'
+						inbox='3793e40e-c090-4412-acd0-7e20a7dc9f8a'
+						title='Welcome to Voyage'
+						subtitle='Ask us anything in the chat window below 😊'
+						primaryColor='#3646F5'
+						position='right'
+						greeting='Hi there! How can I help you?'
+						newMessagePlaceholder='Start typing...'
+						showAgentAvailability={false}
+						agentAvailableText="We're online right now!"
+						agentUnavailableText="We're away at the moment."
+						requireEmailUpfront={false}
+						iconVariant='outlined'
+						baseUrl='https://app.papercups.io'
+						styles={{
+							toggleButton: {
+								width: 60,
+								height: 60
+							},
+							toggleContainer: {
+								zIndex: 1000000,
+								position: 'fixed'
+							},
+							chatContainer: {
+								zIndex: 10000000,
+								position: 'fixed'
+							}
+						}}
+						// Optionally include data about your customer here to identify them
+						customer={
+							profile?.id
+								? {
+										name: profile.fullName,
+										email: profile.email,
+										external_id: profile.id,
+										metadata: {
+											type: 'Shipper',
+											phone: profile.phone,
+											company: profile.company
+										},
+										current_url: currentURL
+								  }
+								: undefined
+						}
+					/>
+				)}
 			</main>
 		</div>
 	);
