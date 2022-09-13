db51bfef1e65af22199bb9b91e87dbc4e5a13eb4
Chipzstar <chipzstar.dev@googlemail.com>
2022-09-13 17:19:23 +0100

    add description to quotes workflow page add helper popup for each tab in settings during signup process

--- apps/carrier-dashboard/containers/settings/Organisation.tsx
+++ apps/carrier-dashboard/containers/settings/Organisation.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import { Button, Center, Container, Group, Loader, Stack, TextInput } from '@mantine/core';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Center, Container, Group, Loader, Popover, Stack, Text, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { editCarrier, updateCarrier } from '../../store/feature/profileSlice';
 import { useDispatch } from 'react-redux';
@@ -7,6 +7,7 @@ import { AppDispatch } from '../../store';
 import { Check, X } from 'tabler-icons-react';
 import { ActivationStatus, Carrier } from '../../utils/types';
 import { notifyError, notifySuccess } from '@voyage-app/shared-utils';
+import { useInterval } from '@mantine/hooks';
 
 interface OrganisationProps {
 	carrierInfo: Carrier;
@@ -14,6 +15,8 @@ interface OrganisationProps {
 }
 
 const Organisation = ({ carrierInfo, nextTab }: OrganisationProps) => {
+	const [opened, setOpen] = useState(true);
+	const interval = useInterval(() => setOpen(true), 3000);
 	const [loading, setLoading] = useState(false);
 	const dispatch = useDispatch<AppDispatch>();
 	const initialValues: Carrier = {
@@ -39,70 +42,85 @@ const Organisation = ({ carrierInfo, nextTab }: OrganisationProps) => {
 		initialValues
 	});
 
-	const handleSubmit = useCallback(values => {
-		setLoading(true);
-		// CHECK IF CARRIER SIGNUP STATUS IS NOT COMPLETE, IF IT IS NOT, change status to WORKFLOWS
-		if (carrierInfo.status === ActivationStatus.COMPANY_INFO) {
-			console.log("STATUS:",carrierInfo.status)
-			values.status = ActivationStatus.WORKFLOWS
-		}
-		dispatch(updateCarrier(values))
-			.unwrap()
-			.then(() => {
-				notifySuccess('update-carrier-success', `Your Organisation details have been saved`, <Check size={20} />);
-				setLoading(false);
-				if (carrierInfo.status === ActivationStatus.COMPANY_INFO) {
-					dispatch(editCarrier({...carrierInfo, status: ActivationStatus.WORKFLOWS }))
-					nextTab();
-				}
-			})
-			.catch(err => {
-				console.error(err);
-				notifyError('update-carrier-error', `There was error updating your organisation details ${err.message}`, <X size={20} />);
-				setLoading(false);
-			});
+	useEffect(() => {
+		carrierInfo.status !== ActivationStatus.COMPLETE && interval.start();
+		return () => interval.stop();
 	}, [carrierInfo]);
+
+	const handleSubmit = useCallback(
+		values => {
+			setLoading(true);
+			// CHECK IF CARRIER SIGNUP STATUS IS NOT COMPLETE, IF IT IS NOT, change status to WORKFLOWS
+			if (carrierInfo.status === ActivationStatus.COMPANY_INFO) {
+				console.log('STATUS:', carrierInfo.status);
+				values.status = ActivationStatus.WORKFLOWS;
+			}
+			dispatch(updateCarrier(values))
+				.unwrap()
+				.then(() => {
+					notifySuccess('update-carrier-success', `Your Organisation details have been saved`, <Check size={20} />);
+					setLoading(false);
+					if (carrierInfo.status === ActivationStatus.COMPANY_INFO) {
+						dispatch(editCarrier({ ...carrierInfo, status: ActivationStatus.WORKFLOWS }));
+						nextTab();
+					}
+				})
+				.catch(err => {
+					console.error(err);
+					notifyError('update-carrier-error', `There was error updating your organisation details ${err.message}`, <X size={20} />);
+					setLoading(false);
+				});
+		},
+		[carrierInfo]
+	);
 
 	return (
 		<Container fluid className='tab-container bg-voyage-background'>
-			<Center className='flex flex-col h-full w-full'>
-				<header className='page-header my-6'>Organisation Settings</header>
-				<form onSubmit={form.onSubmit(handleSubmit)} className="w-256">
-					<Stack className='w-full grow'>
-						<Group grow>
-							<TextInput radius={0} label='Full Name' {...form.getInputProps('fullName')} />
-							<TextInput radius={0} label='Company Name' {...form.getInputProps('company')} />
+			<Popover opened={opened} onChange={setOpen} transition='fade' transitionDuration={500} position='bottom' withArrow shadow='md'>
+				<Center className='flex h-full w-full flex-col'>
+					<header className='page-header my-6'>Organisation Settings</header>
+					<form onSubmit={form.onSubmit(handleSubmit)} className='w-256'>
+						<Stack className='w-full grow'>
+							<Group grow>
+								<TextInput radius={0} label='Full Name' {...form.getInputProps('fullName')} />
+								<TextInput radius={0} label='Company Name' {...form.getInputProps('company')} />
+							</Group>
+							<Group grow>
+								<TextInput radius={0} label='Phone Number' {...form.getInputProps('phone')} />
+								<TextInput radius={0} label='Email Address' {...form.getInputProps('email')} disabled />
+							</Group>
+							<Group grow>
+								<TextInput radius={0} label='Address Line 1' {...form.getInputProps('address.line1')} />
+								<TextInput radius={0} label='Address Line 2' {...form.getInputProps('address.line2')} />
+							</Group>
+							<Group grow>
+								<TextInput radius={0} label='City' {...form.getInputProps('address.city')} />
+								<TextInput radius={0} label='Postal Code' {...form.getInputProps('address.postcode')} />
+							</Group>
+							<Group grow>
+								<TextInput radius={0} label='Region' {...form.getInputProps('address.region')} />
+								<TextInput radius={0} label='Country' readOnly {...form.getInputProps('address.country')} />
+							</Group>
+						</Stack>
+						<Group my={20} py={10}>
+							<Popover.Target>
+								<Button
+									type='submit'
+									classNames={{
+										root: 'bg-secondary hover:bg-secondary-600'
+									}}
+								>
+									<Loader size='sm' className={`mr-3 ${!loading && 'hidden'}`} />
+									<span>Save Changes</span>
+								</Button>
+							</Popover.Target>
+							<Popover.Dropdown>
+								<Text size='sm'>Click "Save Changes" to continue</Text>
+							</Popover.Dropdown>
 						</Group>
-						<Group grow>
-							<TextInput radius={0} label='Phone Number' {...form.getInputProps('phone')} />
-							<TextInput radius={0} label='Email Address' {...form.getInputProps('email')} disabled/>
-						</Group>
-						<Group grow>
-							<TextInput radius={0} label='Address Line 1' {...form.getInputProps('address.line1')} />
-							<TextInput radius={0} label='Address Line 2' {...form.getInputProps('address.line2')} />
-						</Group>
-						<Group grow>
-							<TextInput radius={0} label='City' {...form.getInputProps('address.city')} />
-							<TextInput radius={0} label='Postal Code' {...form.getInputProps('address.postcode')} />
-						</Group>
-						<Group grow>
-							<TextInput radius={0} label='Region' {...form.getInputProps('address.region')} />
-							<TextInput radius={0} label='Country' readOnly {...form.getInputProps('address.country')} />
-						</Group>
-					</Stack>
-					<Group my={20} py={10}>
-						<Button
-							type='submit'
-							classNames={{
-								root: 'bg-secondary hover:bg-secondary-600'
-							}}
-						>
-							<Loader size='sm' className={`mr-3 ${!loading && 'hidden'}`} />
-							<span>Save Changes</span>
-						</Button>
-					</Group>
-				</form>
-			</Center>
+					</form>
+				</Center>
+			</Popover>
 		</Container>
 	);
 };
--- apps/carrier-dashboard/containers/settings/Workflows.tsx
+++ apps/carrier-dashboard/containers/settings/Workflows.tsx
@@ -1,14 +1,15 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useForm } from '@mantine/form';
 import { createSettings, updateSettings } from '../../store/feature/settingsSlice';
 import { Check, X } from 'tabler-icons-react';
-import { Center, Container, Stack, Group, Checkbox, Button, NumberInput } from '@mantine/core';
+import { Center, Container, Stack, Group, Checkbox, Button, NumberInput, Popover, Text } from '@mantine/core';
 import { Carrier, ChargeUnitType, Settings, ActivationStatus } from '../../utils/types';
 import { defaultSettings } from '../../utils/constants';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from 'apps/carrier-dashboard/store';
 import { notifyError, notifySuccess } from '@voyage-app/shared-utils';
-import { editCarrier } from '../../store/feature/profileSlice'
+import { editCarrier } from '../../store/feature/profileSlice';
+import { useInterval } from '@mantine/hooks';
 
 interface WorkflowsProps {
 	carrierInfo: Carrier;
@@ -17,6 +18,8 @@ interface WorkflowsProps {
 }
 
 const Workflows = ({ carrierInfo, settings, nextTab }: WorkflowsProps) => {
+	const [opened, setOpen] = useState(true);
+	const interval = useInterval(() => setOpen(true), 3000);
 	const dispatch = useDispatch<AppDispatch>();
 	const initialValues: Settings = {
 		id: settings?.id ?? undefined,
@@ -27,6 +30,11 @@ const Workflows = ({ carrierInfo, settings, nextTab }: WorkflowsProps) => {
 		initialValues
 	});
 
+	useEffect(() => {
+		carrierInfo.status !== ActivationStatus.COMPLETE && interval.start();
+		return interval.stop();
+	}, [carrierInfo]);
+
 	const submitQuoteSettings = useCallback(
 		values => {
 			settings?.id
@@ -35,7 +43,7 @@ const Workflows = ({ carrierInfo, settings, nextTab }: WorkflowsProps) => {
 						.then(() => {
 							notifySuccess('update-settings-success', 'Quote settings saved!', <Check size={20} />);
 							if (carrierInfo.status === ActivationStatus.WORKFLOWS) {
-								dispatch(editCarrier({...carrierInfo, status: ActivationStatus.BANK_ACCOUNT }))
+								dispatch(editCarrier({ ...carrierInfo, status: ActivationStatus.BANK_ACCOUNT }));
 								nextTab();
 							}
 						})
@@ -53,87 +61,96 @@ const Workflows = ({ carrierInfo, settings, nextTab }: WorkflowsProps) => {
 
 	return (
 		<Container fluid className='tab-container bg-voyage-background'>
-			<Center className='flex h-full flex-col'>
-				<section className='border-voyage-grey flex h-full flex-col items-center justify-center'>
-					<header className='page-header my-6'>Quote Settings</header>
-					<form onSubmit={quoteConfigForm.onSubmit(submitQuoteSettings)}>
-						<Stack>
-							<Group position='center'>
-								<Checkbox
-									checked={quoteConfigForm.values.rateChargeRules[ChargeUnitType.DISTANCE].active}
-									label='Charge per mile'
-									className='lg:w-48'
-									{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.DISTANCE}.active`)}
-								/>
-								<NumberInput
-									disabled={!quoteConfigForm.values.rateChargeRules[ChargeUnitType.DISTANCE].active}
-									required
-									size='sm'
-									radius={0}
-									precision={2}
-									step={0.05}
-									min={0}
-									max={100}
-									placeholder='Charge'
-									{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.DISTANCE}.value`)}
-									icon={<span className='text-voyage-grey'>£</span>}
-								/>
-							</Group>
-							<Group position='center'>
-								<Checkbox
-									checked={quoteConfigForm.values.rateChargeRules[ChargeUnitType.PACKAGE].active}
-									label='Charge per pallet'
-									className='lg:w-48'
-									{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.PACKAGE}.active`)}
-								/>
-								<NumberInput
-									disabled={!quoteConfigForm.values.rateChargeRules[ChargeUnitType.PACKAGE].active}
-									required
-									size='sm'
-									radius={0}
-									precision={2}
-									step={0.05}
-									min={0}
-									max={100}
-									placeholder='Charge'
-									{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.PACKAGE}.value`)}
-									icon={<span className='text-voyage-grey'>£</span>}
-								/>
-							</Group>
+			<Popover opened={opened} onChange={setOpen} transition='fade' transitionDuration={500} position='bottom' withArrow shadow='md'>
+				<Center className='flex h-full flex-col'>
+					<section className='border-voyage-grey flex h-full flex-col items-center justify-center'>
+						<header className='page-header my-6'>Quote Settings</header>
+						<section className='w-128 pb-6 text-sm'>
+							<p className='text-gray-600'>When booking loads, we calculate the final rate based on <strong>distance</strong>, <strong>package quantity</strong> and <strong>package weight</strong>. Here you can adjust how much each variable contributes to the final rate</p>
+						</section>
+						<form onSubmit={quoteConfigForm.onSubmit(submitQuoteSettings)}>
+							<Stack>
+								<Group position='center'>
+									<Checkbox
+										checked={quoteConfigForm.values.rateChargeRules[ChargeUnitType.DISTANCE].active}
+										label='Charge per mile'
+										className='lg:w-48'
+										{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.DISTANCE}.active`)}
+									/>
+									<NumberInput
+										disabled={!quoteConfigForm.values.rateChargeRules[ChargeUnitType.DISTANCE].active}
+										required
+										size='sm'
+										radius={0}
+										precision={2}
+										step={0.05}
+										min={0}
+										max={100}
+										placeholder='Charge'
+										{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.DISTANCE}.value`)}
+										icon={<span className='text-voyage-grey'>£</span>}
+									/>
+								</Group>
+								<Group position='center'>
+									<Checkbox
+										checked={quoteConfigForm.values.rateChargeRules[ChargeUnitType.PACKAGE].active}
+										label='Charge per pallet'
+										className='lg:w-48'
+										{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.PACKAGE}.active`)}
+									/>
+									<NumberInput
+										disabled={!quoteConfigForm.values.rateChargeRules[ChargeUnitType.PACKAGE].active}
+										required
+										size='sm'
+										radius={0}
+										precision={2}
+										step={0.05}
+										min={0}
+										max={100}
+										placeholder='Charge'
+										{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.PACKAGE}.value`)}
+										icon={<span className='text-voyage-grey'>£</span>}
+									/>
+								</Group>
 
-							<Group position='center'>
-								<Checkbox
-									checked={quoteConfigForm.values.rateChargeRules[ChargeUnitType.WEIGHT].active}
-									label='Charge per kg'
-									className='lg:w-48'
-									{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.WEIGHT}.active`)}
-								/>
-								<NumberInput
-									disabled={!quoteConfigForm.values.rateChargeRules[ChargeUnitType.WEIGHT].active}
-									required
-									size='sm'
-									radius={0}
-									precision={2}
-									step={0.05}
-									min={0}
-									max={100}
-									placeholder='Charge'
-									{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.WEIGHT}.value`)}
-									icon={<span className='text-voyage-grey'>£</span>}
-								/>
-							</Group>
-						</Stack>
-						<Stack align='center' py={20}>
-							<Button type='submit' className='bg-secondary hover:bg-secondary-600'>
-								Save Changes
-							</Button>
-						</Stack>
-					</form>
-				</section>
-			</Center>
+								<Group position='center'>
+									<Checkbox
+										checked={quoteConfigForm.values.rateChargeRules[ChargeUnitType.WEIGHT].active}
+										label='Charge per kg'
+										className='lg:w-48'
+										{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.WEIGHT}.active`)}
+									/>
+									<NumberInput
+										disabled={!quoteConfigForm.values.rateChargeRules[ChargeUnitType.WEIGHT].active}
+										required
+										size='sm'
+										radius={0}
+										precision={2}
+										step={0.05}
+										min={0}
+										max={100}
+										placeholder='Charge'
+										{...quoteConfigForm.getInputProps(`rateChargeRules.${ChargeUnitType.WEIGHT}.value`)}
+										icon={<span className='text-voyage-grey'>£</span>}
+									/>
+								</Group>
+							</Stack>
+							<Stack align='center' py={20}>
+								<Popover.Target>
+									<Button type='submit' className='bg-secondary hover:bg-secondary-600'>
+										Save Changes
+									</Button>
+								</Popover.Target>
+								<Popover.Dropdown>
+									<Text size='sm'>Click "Save Changes" to continue</Text>
+								</Popover.Dropdown>
+							</Stack>
+						</form>
+					</section>
+				</Center>
+			</Popover>
 		</Container>
 	);
 };
-
 
 export default Workflows;
