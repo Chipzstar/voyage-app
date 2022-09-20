c389ae9e9e83bfd14caf1730d19aacdbd3a8808c
Chipzstar <chipzstar.dev@googlemail.com>
2022-09-20 16:44:42 +0100

    attempt fix intercom issue

--- apps/carrier-dashboard/context/IntercomProvider.tsx
+++ apps/carrier-dashboard/context/IntercomProvider.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { APP_ID } from '../utils/intercom';
-import { loadIntercom, updateIntercom } from 'next-intercom';
+import { initIntercomWindow, loadIntercom, updateIntercom } from 'next-intercom';
 import { intercomPlatform } from '@voyage-app/shared-utils';
 
 export const IntercomProvider = ({ session, children }) => {
@@ -18,14 +18,24 @@ export const IntercomProvider = ({ session, children }) => {
 			initWindow: true, // default: true
 			delay: 0, // default: 0  - usefull for mobile devices to prevent blocking the main thread
 		});
-		/*bootIntercom({
-			api_base: "https://api-iam.intercom.io",
-			user_id: session?.id,
+	} else {
+		loadIntercom({
+			appId: process.env.NEXT_PUBLIC_INTERCOM_APP_ID, // default : ''
 			name: session?.user.name,
 			email: session?.user.email,
-		});*/
+			user_id: session?.id,
+			ssr: true, // default: false
+			initWindow: false, // default: true
+			delay: 0 // default: 0  - useful for mobile devices to prevent blocking the main thread
+		});
+		initIntercomWindow({
+			appId: process.env.NEXT_PUBLIC_INTERCOM_APP_ID,
+			name: session?.user.name,
+			email: session?.user.email,
+			user_id: session?.id
+		});
 	}
-
+	
 	useEffect(() => {
 		const handleRouteChange = url => {
 			if (typeof window !== 'undefined') {
